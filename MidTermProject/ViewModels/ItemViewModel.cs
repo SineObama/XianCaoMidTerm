b5ea7047906593a7c0f072916934c95d5f504ec3
SineObamaTest <acgkmpoty5@163.com>
2016-05-07 21:53:15 +0800

    testing

--- MidTermProject/ViewModels/ItemViewModel.cs
+++ MidTermProject/ViewModels/ItemViewModel.cs
@@ -64,7 +64,6 @@ namespace MidTermProject.ViewModels
             //}
 
             using (var statement = conn.Prepare("COMMIT TRANSACTION")) { statement.Step(); }
-
         }
 
         public void updateWithHtml(string html)
